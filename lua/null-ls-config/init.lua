c8febd51a16f22ca09d16f0e16881add045c290f
Arturo Farias <pedro.farias.sosa@gmail.com>
2022-05-07 00:05:27 -0500

    fix for null ls

--- lua/null-ls-config/init.lua
+++ lua/null-ls-config/init.lua
@@ -4,17 +4,17 @@ local formatting = null_ls.builtins.formatting
 null_ls.setup({
   sources = {
     null_ls.builtins.code_actions.eslint,
-    formatting.prettier, formatting.black, formatting.gofmt, formatting.shfmt,
-    formatting.clang_format, formatting.cmake_format, formatting.dart_format,
-    formatting.lua_format.with({
-      extra_args = {
-        '--no-keep-simple-function-one-line', '--no-break-after-operator', '--column-limit=100',
-        '--break-after-table-lb', '--indent-width=2'
-      }
-    }), formatting.isort, formatting.codespell.with({filetypes = {'markdown'}})
+    -- formatting.prettier, formatting.black, formatting.gofmt, formatting.shfmt,
+    -- formatting.clang_format, formatting.cmake_format, formatting.dart_format,
+    -- formatting.lua_format.with({
+    --   extra_args = {
+    --     '--no-keep-simple-function-one-line', '--no-break-after-operator', '--column-limit=100',
+    --     '--break-after-table-lb', '--indent-width=2'
+    --   }
+    -- }), formatting.isort, formatting.codespell.with({filetypes = {'markdown'}})
   },
   on_attach = function(client)
-        if client.resolved_capabilities.document_formatting then
+        if client.server_capabilities.document_formatting then
             vim.cmd([[
             augroup LspFormatting
                 autocmd! * <buffer>
